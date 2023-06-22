0031c1470640f75ab6ad19f9d2cc46f89cf61e92
Seligin Denis <seligindenis@yandex.ru>
2023-06-22 21:29:58 +0300

    add 100 likes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val likes = 21
-    val textPeople = if (likes == 1 || (likes % 10 == 1 && likes != 11)) "человеку" else "людям"
+    val likes = 1001
+    val textPeople = if (likes % 10 == 1 || likes % 100 == 11) "человеку" else "людям"
 
     println("Понравилось $likes $textPeople")
 }
