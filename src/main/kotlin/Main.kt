0dbd48b68147dece2767bb4c119d5af1d7166d81
Seligin Denis <seligindenis@yandex.ru>
2023-06-23 16:17:41 +0300

    changed textPeople

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val likes = 1001
-    val textPeople = if (likes % 10 == 1 || likes % 100 == 11) "человеку" else "людям"
+    val likes = 3011
+    val textPeople = if (likes % 10 == 1 && likes % 100 != 11) "человеку" else "людям"
 
     println("Понравилось $likes $textPeople")
 }
